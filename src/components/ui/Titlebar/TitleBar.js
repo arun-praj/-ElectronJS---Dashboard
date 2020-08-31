9c624eea6396c8e0d328b79c6f322fcb35235095
Arun K. Prajapati <arunkp1122@gmail.com>
2020-08-31 20:17:24 +0545

    [Fix] Hide titlebar for windows

--- src/components/ui/Titlebar/TitleBar.js
+++ src/components/ui/Titlebar/TitleBar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { is } from "electron-util";
 import "./TitleBar.scss";
 const TitleBar = () => {
+   console.log(is.macos);
+   // is.macos
    return (
-      <div
-         className='titlebar'
-         style={{ backgroundColor: "rgb(22,23,24)", color: "#ffffff", textAlign: "center" }}></div>
+      is.macos && (
+         <div
+            className='titlebar'
+            style={{
+               backgroundColor: "rgb(22,23,24)",
+               color: "#ffffff",
+               textAlign: "center",
+            }}></div>
+      )
    );
 };
 export default TitleBar;
--- webpack.build.config.js
+++ webpack.build.config.js
@@ -10,7 +10,7 @@ module.exports = {
       rules: [
          {
             test: /\.scss$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader"],
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
          },
          {
             test: /\.jsx?$/,
